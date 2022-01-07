4162d5a13fbf809263afaf3cb6dbf20f7fa1479e
Marco <marco.mora88@gmail.com>
2022-01-07 11:44:39 +0100

    small refactoring

--- text_cleaning.py
+++ text_cleaning.py
@@ -7,16 +7,16 @@ def remove_newlines(string):
     string = string.replace('\n', ' ')
     return string.replace('. .', '')
 
-def remove_words(words_to_remove):
+def remove_words(regex_to_remove):
     def run(string):
-        for word in words_to_remove:
-            string = re.sub(word, '', string)
+        for regex in regex_to_remove:
+            string = re.sub(regex, '', string)
         
         return string
     
     return run
 
-def clean_symbols(string, encoding='utf-8'):
+def clean_symbols(string):
     string = string.replace("’", "'")
     return re.sub(r'[^\w .\'#@]', '', string)
 
